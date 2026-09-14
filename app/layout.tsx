1d9cd1fdcbc4b8f0aea9a7b7a8cf598c96575411
WasfiS <50295773+WasfiS@users.noreply.github.com>
2026-09-14 17:58:03 +0200

    Add Google Ads tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -46,6 +47,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
+        <GoogleTagManager gtmId="AW-18449412545" />
       </body>
     </html>
   );
